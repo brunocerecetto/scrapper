ef52353f95a1fa2f100d6f49890619a57b6307f2
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2020-04-21 19:32:28 -0300

    adding dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,22 @@
-import * as bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import App from './app';
-
 import loggerMiddleware from './middlewares/logger';
-
 import HomeController from './controllers/home/home.controller';
 
+import 'dotenv/config';
+
+const port: number = (process.env.PORT as unknown) as number;
+
 const app = new App({
-  port: 5000,
+  port,
   controllers: [new HomeController()],
-  middleWares: [loggerMiddleware],
+  middleWares: [
+    bodyParser.json({ limit: '50mb' }),
+    bodyParser.urlencoded({ limit: '50mb', extended: true }),
+    loggerMiddleware,
+    cors(),
+  ],
 });
 
 app.listen();
